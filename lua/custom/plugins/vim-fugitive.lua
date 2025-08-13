efb46b7770785f88f2926bc24656b28704837fe2
dvir-highlander <dvir@highlander.io>
2025-08-13 14:49:18 +0300

    Adding desc fro p AND P

--- lua/custom/plugins/vim-fugitive.lua
+++ lua/custom/plugins/vim-fugitive.lua
@@ -1,22 +1,28 @@
 return {
-  "tpope/vim-fugitive",
+  'tpope/vim-fugitive',
   config = function()
     -- Global keymap to open fugitive
-    vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "[G]it [S]tatus" })
+    vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = '[G]it [S]tatus' })
 
     -- Autocommand to create keymaps only in fugitive buffers
-    local fugitive_augroup = vim.api.nvim_create_augroup("FugitiveKeymaps", { clear = true })
-    vim.api.nvim_create_autocmd("BufWinEnter", {
+    local fugitive_augroup = vim.api.nvim_create_augroup('FugitiveKeymaps', { clear = true })
+    vim.api.nvim_create_autocmd('BufWinEnter', {
       group = fugitive_augroup,
-      pattern = "*",
+      pattern = '*',
       callback = function()
-        if vim.bo.ft ~= "fugitive" then
+        if vim.bo.ft ~= 'fugitive' then
           return
         end
         local bufnr = vim.api.nvim_get_current_buf()
         local opts = { buffer = bufnr, remap = false, silent = true }
-        vim.keymap.set("n", "<leader>p", function() vim.cmd.Git('push') end, opts)
-        vim.keymap.set("n", "<leader>P", function() vim.cmd.Git({'pull', '--rebase'}) end, opts)
+        local push_opts = { buffer = bufnr, remap = false, silent = true, desc = '[P]ush to remote' }
+        local pull_opts = { buffer = bufnr, remap = false, silent = true, desc = '[P]ull with rebase' }
+        vim.keymap.set('n', '<leader>p', function()
+          vim.cmd.Git 'push'
+        end, push_opts)
+        vim.keymap.set('n', '<leader>P', function()
+          vim.cmd.Git { 'pull', '--rebase' }
+        end, pull_opts)
       end,
     })
   end,
